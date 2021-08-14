31c3df5b714cc214adff913e2af5ede5d4a2325f
Redouane S <redouane.sobaihi@live.com>
2021-08-14 16:22:54 +0700

    feat(project): project code convention and structure - add husky for git commit hooks - add commit message lint hooks - add hapi-js packages and create simple server - add dotenv and .env configurations

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
+import { config } from 'dotenv'
+config()
+
+// eslint-disable-next-line import/first
+import { Server } from '@hapi/hapi'
 
 (async function (): Promise<void> {
-  console.log('Hello!')
+  const server = new Server({
+    host: process.env.HOST,
+    port: process.env.PORT
+  })
+
+  await server.start()
 })()
---
+++ test/index.test.ts
@@ -0,0 +1,10 @@
+import Lab from '@hapi/lab'
+import { expect } from '@hapi/code'
+
+const { describe, it } = exports.lab = Lab.script()
+
+describe('test', () => {
+  it('test', () => {
+    expect(true)
+  })
+})
