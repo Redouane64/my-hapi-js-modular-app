e3d0c0a479045e42f888aa4fdd59ce389b635d9a
Redouane S <redouane.sobaihi@live.com>
2021-08-16 10:18:27 +0700

    feat(server): register api and database module

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,7 @@
 import * as Pino from 'hapi-pino'
 import { Server } from '@hapi/hapi'
+import { Api } from '../api'
+import { Database } from '../database'
 
 export const createServer = async (): Promise<Server> => {
   const server = new Server({
@@ -12,6 +14,14 @@ export const createServer = async (): Promise<Server> => {
     options: {
       prettyPrint: true
     }
+  })
+
+  await server.register({
+    plugin: Database
+  })
+
+  await server.register({
+    plugin: Api
   })
 
   return server
